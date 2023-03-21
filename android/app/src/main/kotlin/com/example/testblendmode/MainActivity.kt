154e3b1abbb9ce57b108ac31cdaa3a7ca227388e
Quan Ngo <nvquan@fossil.com>
2023-03-21 22:28:22 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/testblendmode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testblendmode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blend Mode Test',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Blend Mode Test'),
+        ),
+        body: BlendModeTest(),
+      ),
+    );
+  }
+}
+
+class BlendModeTest extends StatelessWidget {
+  final List<BlendMode> blendModes = BlendMode.values;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("source"),
+            Container(width: 200, height: 200, color: Colors.white,),
+            Text("destination"),
+            Image.asset(
+              'assets/sample.png',
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 20,),
+            ...blendModes.map((mode) => blendModeExample(mode)).toList()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget blendModeExample(BlendMode mode) {
+    return Column(
+      children: [
+        SizedBox(height: 20),
+        Text(mode.toString()),
+        SizedBox(height: 20),
+        Image.asset(
+          'assets/sample.png',
+          color: Colors.white,
+          colorBlendMode: mode,
+          width: 200,
+          height: 200,
+        )
+      ],
+    );
+  }
+}
